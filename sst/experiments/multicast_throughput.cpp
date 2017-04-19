de710b94debb9ce6f31c1a414091100a4f7ad5d3
Sagar Jha <sjsagarjha3@gmail.com>
2017-04-19 19:10:30 -0400

    Had forgotten to create the timeout thread in throughput

--- sst/experiments/multicast_throughput.cpp
+++ sst/experiments/multicast_throughput.cpp
@@ -1,5 +1,7 @@
+#include <chrono>
 #include <iostream>
 #include <memory>
+#include <thread>
 #include <vector>
 
 #include "derecho/experiments/aggregate_bandwidth.h"
@@ -12,9 +14,10 @@ using namespace sst;
 
 volatile bool done = false;
 
-int main() {
+int main(int argc, char *argv[]) {
     constexpr uint max_msg_size = 1, window_size = 1000;
     const unsigned int num_messages = 1000000;
+    int num_senders_selector = atoi(argv[1]);
     // input number of nodes and the local node id
     uint32_t node_id, num_nodes;
     cin >> node_id >> num_nodes;
@@ -38,21 +41,33 @@ int main() {
             members, node_id),
         window_size);
 
+    auto check_failures_loop = [&sst]() {
+        pthread_setname_np(pthread_self(), "check_failures");
+        while(true) {
+            std::this_thread::sleep_for(chrono::microseconds(100));
+            if(sst) {
+                sst->put_with_completion((char*)std::addressof(sst->heartbeat[0]) - sst->getBaseAddress(), sizeof(bool));
+            }
+        }
+    };
+
+    thread failures_thread = std::thread(check_failures_loop);
+
     uint num_finished = 0;
-    auto sst_receive_handler = [&num_finished, &num_nodes, &num_messages](
-                                           uint32_t sender_rank, uint64_t index,
-                                           volatile char* msg, uint32_t size) {
-            if(index == num_messages - 1) {
-                num_finished++;
-            }
-            if(num_finished == num_nodes) {
-                done = true;
-            }
+    auto sst_receive_handler = [&num_finished, num_senders_selector, &num_nodes, &num_messages](
+        uint32_t sender_rank, uint64_t index,
+        volatile char* msg, uint32_t size) {
+        if(index == num_messages - 1) {
+            num_finished++;
+        }
+        if(num_finished == num_nodes || (num_senders_selector == 1 && num_finished == num_nodes / 2) || (num_senders_selector == 2 && num_finished == 1)) {
+            done = true;
+        }
     };
     auto receiver_pred = [](const multicastSST&) {
         return true;
     };
-    auto num_times = window_size / 2;
+    auto num_times = window_size / num_nodes;
     if(!num_times) {
         num_times = 1;
     }
@@ -78,22 +93,24 @@ int main() {
         }
     };
     sst->predicates.insert(receiver_pred, receiver_trig,
-                                                  sst::PredicateType::RECURRENT);
+                           sst::PredicateType::RECURRENT);
 
     struct timespec start_time, end_time;
     sst_multicast_group<multicastSST> g(sst, window_size);
     // uint count = 0;
     // start timer
     clock_gettime(CLOCK_REALTIME, &start_time);
-    for(uint i = 0; i < num_messages; ++i) {
-        volatile char* buf;
-        while((buf = g.get_buffer(max_msg_size)) == NULL) {
-	  // ++count;
+    if(node_id == 0 || num_senders_selector == 0 || (node_id > (num_nodes - 1) / 2 && num_senders_selector == 1)) {
+        for(uint i = 0; i < num_messages; ++i) {
+            volatile char* buf;
+            while((buf = g.get_buffer(max_msg_size)) == NULL) {
+                // ++count;
+            }
+            // for(uint i = 0; i < size; ++i) {
+            //     buf[i] = 'a' + rand() % 26;
+            // }
+            g.send();
         }
-        // for(uint i = 0; i < size; ++i) {
-        //     buf[i] = 'a' + rand() % 26;
-        // }
-        g.send();
     }
     // cout << "Done sending" << endl;
     while(!done) {
@@ -103,11 +120,19 @@ int main() {
     double my_time = ((end_time.tv_sec * 1e9 + end_time.tv_nsec) -
                       (start_time.tv_sec * 1e9 + start_time.tv_nsec));
     double message_rate = (num_messages * 1e9) / my_time;
+    ;
+    if(num_senders_selector == 0) {
+        message_rate *= num_nodes;
+    } else if(num_senders_selector == 1) {
+        message_rate *= num_nodes / 2;
+    }
+
     // cout << "Time in nanoseconds " << my_time << endl;
     // cout << "Number of messages per second " << message_rate
     //      << endl;
     // cout << "Number of times null returned, count = " << count << endl;
     double sum_message_rate =
-        aggregate_bandwidth(members, node_id, message_rate * num_nodes);
-    log_results(num_nodes, max_msg_size, sum_message_rate, "data_multicast");
+        aggregate_bandwidth(members, node_id, message_rate);
+    log_results(num_nodes, num_senders_selector, max_msg_size, sum_message_rate, "data_multicast");
+    sst->sync_with_members();
 }
